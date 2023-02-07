2678cbcfb8dc76cd295834cd9cb0688959448e40
anphu123 <anphu221002@gmail.com>
2023-02-08 03:38:48 +0700

    phu

---
+++ app/src/main/java/com/example/quanlyduanagilenhom_cp17201/intro.java
@@ -0,0 +1,24 @@
+package com.example.quanlyduanagilenhom_cp17201;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class intro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ConstraintLayout startBtn=findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(intro.this,MainActivity.class));
+            }
+        });
+    }
+}
