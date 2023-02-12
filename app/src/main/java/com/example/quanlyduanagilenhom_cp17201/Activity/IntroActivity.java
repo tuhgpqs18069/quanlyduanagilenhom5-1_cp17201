c6d4aba1a8ddb98119d74be0489d892fa61ef906
Tú <tuhgqps18069@fpt.edu.vn>
2023-02-12 17:53:40 +0700

    Update IntroActivity.java

--- app/src/main/java/com/example/quanlyduanagilenhom_cp17201/Activity/IntroActivity.java
+++ app/src/main/java/com/example/quanlyduanagilenhom_cp17201/Activity/IntroActivity.java
@@ -14,7 +14,7 @@ public class IntroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intro);
         ConstraintLayout startBtn=findViewById(R.id.startBtn);
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
