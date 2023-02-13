9a01c1e2185fc70d5830478448608cac87898378
Tú <tuhgqps18069@fpt.edu.vn>
2023-02-13 13:05:49 +0700

    huynhtu

---
+++ app/src/main/java/com/example/quanlyduanagilenhom_cp17201/Activity/Activity/CartListActivity.java
@@ -0,0 +1,18 @@
+package com.example.quanlyduanagilenhom_cp17201.Activity.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quanlyduanagilenhom_cp17201.R;
+
+public class CartListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_list);
+
+
+    }
+}
